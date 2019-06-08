44a066577eb3ab83f4be5c50024d31344e416c1c
Nicolas <nicolas0915@hotmail.com>
2019-06-08 09:51:53 -0500

    Header

--- source/js/home.js
+++ source/js/home.js
@@ -1,6 +1,9 @@
 function getSize()
 {
     let windowHeight = window.innerHeight;
+    let windowWidth = window.innerWidth;
+    
     document.getElementById("hero").style.height = windowHeight + "px";
+    document.getElementById("hero").style.width = windowWidth + "px";
 }
 
