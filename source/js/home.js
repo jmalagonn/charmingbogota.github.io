66b8968191f8b8dc4bb825db1abde97217999076
Nicolas <nicolas0915@hotmail.com>
2019-06-10 23:31:41 -0500

    Home2

--- source/js/home.js
+++ source/js/home.js
@@ -1,6 +1,12 @@
 function getSize()
 {
-    let windowHeight = window.innerHeight;    
-    document.getElementById("hero").style.height = windowHeight + "px";   
+    let windowHeight = window.innerHeight;
+    let windowWidth = window.innerWidth;
+    console.log(windowWidth < 575.98);
+    document.getElementById("hero").style.height = windowHeight + "px";
+    
+    if(windowWidth < 575.98){
+        document.getElementById("nav").classList.add("justify-content-center");
+    }
 }
 
