6610cca65794df1c8273d9e06ca0d04ccfc07b6f
Nicolas M <nmalagon@aplyca.com>
2019-06-12 18:39:36 -0500

    Menu

--- source/js/home.js
+++ source/js/home.js
@@ -1,18 +1,21 @@
-function getSize()
-{
+function getSize() {
     let windowHeight = window.innerHeight;
-    let windowWidth = window.innerWidth;
-    console.log(windowWidth < 575.98);
-    document.getElementById("hero").style.height = windowHeight + "px";
-    
-    if(windowWidth < 575.98){
-        document.getElementById("nav").classList.add("justify-content-center");
+
+    if( document.getElementById("hero") != null ){
+        document.getElementById("hero").style.height = windowHeight + "px";
     }
 
-    if(windowWidth < 600){
-        document.getElementById("breakfast-promo").style.width = windowWidth - 100 + "px";
+    if (document.getElementById("inner-menu") != null) {
+        document.getElementById("inner-menu").style.height = windowHeight + "px";
+    }
+}
+
+function showMenu() {
+    let element = document.getElementById("inner-menu");
+    if (element.style.display === "none"){
+        element.style.display = "block";
     }else{
-        document.getElementById("breakfast-promo").style.width = 600 + "px";
+        element.style.display = "none";
     }
 }
 
