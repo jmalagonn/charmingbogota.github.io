1668328aae882230282ee803708efb2ccc78f93e
Nicolas M <nmalagon@aplyca.com>
2019-06-17 19:32:28 -0500

    patternlab

--- source/js/home.js
+++ source/js/home.js
@@ -1,22 +1,37 @@
 function getSize() {
     let windowHeight = window.innerHeight;
-    document.getElementById("inner-menu").style.display = "none";
+    
+    if( $("#inner-menu") != null)
+        $("#inner-menu").css({ 'display':'none'}); 
 
-    if( document.getElementById("hero") != null ){
-        document.getElementById("hero").style.height = windowHeight + "px";
+    if( $("#hero") != null ){
+        $("#hero").css({ 'height' : windowHeight + 'px'});
     }
 
-    if (document.getElementById("inner-menu") != null) {
-        document.getElementById("inner-menu").style.height = windowHeight + "px";
+    if ($("#inner-menu") != null) {
+        $("#inner-menu").css({ 'height': windowHeight + 'px'});
+    }
+
+    if ($("#detail-breakfast") != null) {
+        $("#detail-breakfast").css({ 'display': 'none' });
+    }
+
+    if ($("#additionals-block") != null) {
+        $("#additionals-block").css({ 'display': 'none' });
     }
 }
 
-function showMenu() {
-    let element = document.getElementById("inner-menu");
-    if (element.style.display === "none"){
-        element.style.display = "block";
-    }else{
-        element.style.display = "none";
+function show(id) {
+    $("#" + id).css('display') === 'none' ? $("#" + id).css({ 'display': 'block' }) : $("#" + id).css({ 'display': 'none' });
+}
+
+function showAdditionals(cond) {
+    if ($("#additionals-block").css('display') === 'none' && cond === 'yesAddition'){
+        $("#additionals-block").css({ 'display': 'block' })
+    } else if($("#additionals-block").css('display') === 'block' && cond === 'yesAddition'){
+        $("#additionals-block").css({ 'display': 'block' });
+    } else {
+        $("#additionals-block").css({ 'display': 'none' });
     }
 }
 
